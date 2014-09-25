ef30f606ffbb023e70bd5cb8fabe53e37b281542
Jacek Tomaszewski <jacek@jtom.me>
2014-09-25 14:52:41 +0200

    Update to new bespoke

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,28 @@
-bespoke.from('article', {
-  keys: true,
-  touch: true,
-  bullets: '.bullet, .bullets > *',
-  scale: true,
-  hash: true,
-  progress: true,
-  state: true
-});
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  cube = require('bespoke-theme-cube'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress'),
+  state = require('bespoke-state');
+
+// Bespoke.js
+bespoke.from('article', [
+  cube(),
+  keys(),
+  touch(),
+  bullets('.bullet, .bullets > *'),
+  scale(),
+  hash(),
+  progress(),
+  state()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
